478014bddc68d2aa97430acb7549158f5d836d72
Giovanny Prens Luna <69285209+GPrensLuna@users.noreply.github.com>
2024-07-30 16:18:41 -0500

    correciones

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,15 @@
-import {server} from "./src/server";
-import {PORT} from "./src/config";
+import { server } from "./src/server";
+import { PORT } from "./src/config";
 
 const serverON = () => {
-try {
-    server.listen("4000",()=>{
-        console.log(`server run PORT : ${PORT}`);
-        console.log(`http://localhost:${PORT}`);
-    });    
-    
-} catch (error) {
-    console.error(`server off erro ${error}`)
-}
+  try {
+    server.listen(PORT, () => {
+      console.log(`server run PORT : ${PORT}`);
+      console.log(`http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(`server off erro ${error}`);
+  }
 };
 
 serverON();
