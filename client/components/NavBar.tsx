ad667dd6cd885335a7cf9b69c0c4f777906c23a2
Giovanny Prens Luna <69285209+GPrensLuna@users.noreply.github.com>
2024-07-30 19:44:32 -0500

    correcion del navbar

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -6,39 +6,29 @@ import { useRouter } from 'next/navigation';
 
 const NavBar = () => {
   const router = useRouter();
-  const [mostrarOpciones, setMostrarOpciones] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(!localStorage.getItem('user'));
-  console.log(isLoggedIn)
-  // Estado para verificar si el usuario está logueado
-  const [userName, setUserName] = useState<string | null>(null); // Estado para almacenar el nombre de usuario
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userName, setUserName] = useState<string | null>(null);
 
   useEffect(() => {
-    // Verificar si el usuario está logueado usando localStorage
     const user = localStorage.getItem('user');
     if (user) {
       try {
         const userData = JSON.parse(user);
         setIsLoggedIn(true);
-        setUserName(userData.firstName); // Suponiendo que el nombre del usuario está en la propiedad 'name'
+        setUserName(userData.firstName);
       } catch (error) {
         console.error('Error parsing user data:', error);
-        // En caso de error, limpiar localStorage
         localStorage.removeItem('user');
       }
     }
   }, []);
 
-  const toggleOpciones = () => {
-    setMostrarOpciones(!mostrarOpciones);
-  };
 
   const handleLogout = () => {
-    // Limpiar localStorage y establecer isLoggedIn como falso
     localStorage.removeItem('user');
     setIsLoggedIn(false);
     setUserName(null);
-    router.push("/Usuario")
+    router.push("/Usuario");
   };
 
   return (
@@ -55,17 +45,14 @@ const NavBar = () => {
         <NavLink href="/Paquete">Tienda</NavLink>
         {isLoggedIn ? (
           <>
-            <NavLink href="">{userName}</NavLink>
-            <NavLink href="" onClick={handleLogout}>Salir</NavLink>
+            <NavLink href="#">{userName}</NavLink>
+            <NavLink href="#" onClick={handleLogout}>Salir</NavLink>
           </>
         ) : (
           <NavLink href="/Usuario">Usuario</NavLink>
         )}
         <div className="relative">
-         
-         
-        <NavLink href="/Carrito">Carrito</NavLink>
-
+          <NavLink href="/Carrito">Carrito</NavLink>
         </div>
       </div>
     </nav>
